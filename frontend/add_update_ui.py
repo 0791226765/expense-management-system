b65032f322d7abfb2f46de7037b1c568da272855
Victor Langat <viclangat5@gmail.com>
2025-03-27 15:56:18 +0300

    updated add/update ui

--- frontend/add_update_ui.py
+++ frontend/add_update_ui.py
@@ -19,7 +19,8 @@ def add_update_tab():
         st.error("Failed to retrieve expenses")
         existing_expenses = []
 
-    categories = ["Rent", "Food", "Shopping", "Entertainment", "Other"]
+    categories = ["Rent", "Food", "Shopping", "Entertainment","School fees","Debt Repayment",
+                  "Upkeep","Farmhand","Maid","Transport & Lunch","Church","Gas & Stima","Airtime","Other"]
     with st.form(key="expense_form"):
         col1, col2, col3 = st.columns(3)
         with col1:
@@ -30,7 +31,7 @@ def add_update_tab():
             st.text("Notes")
 
         expenses = []
-        for i in range(5):
+        for i in range(10):
             if i < len(existing_expenses):
                 amount = existing_expenses[i]['amount']
                 category = existing_expenses[i]["category"]
